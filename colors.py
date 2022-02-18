0b6f17a91bf843a78e1a748d81bb6ffc7ade62f9
Alex <amigoalex@yandex.ru>
2022-02-18 17:38:31 +0200

    Update colors.py

--- colors.py
+++ colors.py
@@ -9,29 +9,29 @@ text_task = "" # Рандомное задание
 a1 = 6 # Эти переменные нужны на старте, чтобы программа не удаляла элементы внутри массива
 a2 = 6 #
 
+# Функция рандома всех кнопок, текста и цвета
 def random_task():
     global col, col_text, text_task
-    col = ['red', 'green', 'blue', 'orange', 'black']
+    col = ['red', 'green', 'blue', 'orange', 'black']  # Списки из которых рандомно выбирается
     col_text = ['red', 'green', 'blue', 'orange', 'black']
     text_task = colors[random.randint(0, len(colors) - 1)] # рандомное задание
     lab.config(bg=text_task)
-
+    # Рандом цвета кнопки
     def random_color():
-        if a2 < 5: # Если предыдущий результат уже получен
-            col_text.pop(a2) #  удаляем этот вариант из массива
+        if a2 < 5: #  # удаляю из списка использованный текст, выбранный в random_text()
+            col_text.pop(a2) #  
         num = random.randint(0, len(col) - 1)
         global a1 # Не знаю как сделать без этой задротской ерунды
         a1 = num
         return num
-
+    # Рандом текста на кнопке
     def random_text():
-        if a1 < 5:
+        if a1 < 5: # удаляю из списка использованный цвет, выбранный в random_color()
             col.pop(a1)
         num = random.randint(0, len(col_text) - 1)
         global a2
         a2 = num
         return num
-
 
     # Создаем рандомные кнопки
     but.config(bg=col[random_color()], text=col_text[random_text()])
@@ -41,7 +41,7 @@ def random_task():
     but4.config(bg=col[random_color()], text=col_text[random_text()])
 
     def abs(): # муторная хуйня, чтобы по завершению формирования всех кнопок "обнулить" а2, чтобы в первой функции иф не сработал
-        global a2
+        global a2 # бред, но просто без функции не получается присвоить значение для а2
         a2 = 6
     abs()
 
